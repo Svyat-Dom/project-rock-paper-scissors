da5bd0e634dfd4862ee24ea5370e19a265fd413f
Svyat-Dom <218468265+Svyat-Dom@users.noreply.github.com>
2025-10-23 15:43:59 +0300

    done)

--- javascript.js
+++ javascript.js
@@ -7,17 +7,87 @@ function getComputerChoice() {
     } else {
         pc = 'Scissors';
     }
-    console.log(pc);
-
+    console.log(`PC choice: ${pc}`);
+    return pc;
 }
-getComputerChoice();
 
 function getHumanChoice() {
-    let people = prompt('Rock, Paper, Scissors?', '');
-    console.log(people);
+    let faq = prompt('Rock, Paper, Scissors?', '');
+    let answer = faq.charAt(0).toUpperCase() + faq.slice(1).toLowerCase();
+    console.log(`Your choice: ${answer}`);
+    return answer;
 }
-getHumanChoice();
+
+function playRound(computerChoice, humanChoice) {
+
+    if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
+        console.log(`You lose! Rock win Scissors!`)
+        return computerScore++;
+    }
+    if (computerChoice === 'Paper' && humanChoice === 'Rock') {
+        console.log(`You lose! Paper win Rock!`)
+        return computerScore++;
+    }
+    if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
+        console.log(`You lose! Scissors win Paper!`)
+        return computerScore++;
+    }
+
+    if (computerChoice === 'Rock' && humanChoice === 'Paper') {
+        console.log(`You won! Paper win Rock!`)
+        return humanScore++;
+    }
+    if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
+        console.log(`You won! Scissors win Paper!`)
+        return humanScore++;
+    }
+    if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
+        console.log(`You won! Rock win Scissors!`)
+        return humanScore++;
+    }
+        
+    if (computerChoice === 'Rock' && humanChoice === 'Rock') {
+        console.log(`Draw`)
+    }
+    if (computerChoice === 'Paper' && humanChoice === 'Paper') {
+        console.log(`Draw`)
+    }
+    if (computerChoice === 'Scissors' && humanChoice === 'Scissors') {
+        console.log(`Draw`)
+    }
+}
+
+function playGame () {
+
+    let computerSelection = getComputerChoice();
+    let humanSelection = getHumanChoice();
+
+    playRound(computerSelection, humanSelection);
+    console.log(`PC total: ${computerScore}`);
+    console.log(`Your total: ${humanScore}`);
+    console.log('');
+}
+
+function game () {
+    for (let i = gameRounds; i > 0; i--) {
+        console.log('Round:' + ' ' + roundNumber++);
+        playGame();
+    }
+
+    if ( humanScore > computerScore) {
+        console.log('You Win!');
+    } else if (humanScore < computerScore) {
+        console.log('You Lose!');
+    } else {
+        console.log('Draw!');
+    }
+}
+
 
 let humanScore = 0;
 let computerScore = 0;
 
+let gameRounds = 5;
+let roundNumber = 1;
+
+game();
